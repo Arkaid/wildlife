094b4336b8f81a7f34c39be58e2bcb0e7e8515ea
Francisco Menendez <menendez.f@cave.co.jp>
2017-07-19 11:08:06 +0900

    Finished editor tool to create character files

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -6,145 +6,286 @@ using System.IO;
 namespace Jintori
 {
     // --- Class Declaration ------------------------------------------------------------------------
-    public class CharacterData
+    /// <summary>
+    /// Contains the character sheet info for the select menu
+    /// This is a texture with a few sprites on them, including icon, name, avatar, etc
+    /// </summary>
+    public class CharacterSheet
     {
         // --- Events -----------------------------------------------------------------------------------
         // --- Constants --------------------------------------------------------------------------------
-        const byte Version1 = 1;
+        /// <summary> Width of the sprite sheet </summary>
+        const int ImageWidth = 1200;
 
-        struct Header
-        {
-            public byte version;
-            public int round1Base;
-            public int round1Shadow;
-            public int round2Base;
-            public int round2Shadow;
-            public int round3Base;
-            public int round3Shadow;
-
-            public void Load(BinaryReader br)
-            {
-                version = br.ReadByte();
-
-            }
-        }
-
-        public struct RoundData
-        {
-            public Texture2D baseImage;
-            public Texture2D shadowImage;
-
-            public RoundData(byte[] pngBase, byte[] pngShadow)
-            {
-                baseImage = new Texture2D(PlayArea.ImageWidth, PlayArea.ImageHeight, TextureFormat.ARGB32, false);
-                baseImage.LoadImage(pngBase);
-                baseImage.filterMode = FilterMode.Point;
-
-                shadowImage = new Texture2D(PlayArea.ImageWidth, PlayArea.ImageHeight, TextureFormat.Alpha8, false);
-                shadowImage.LoadImage(pngShadow);
-                shadowImage.filterMode = FilterMode.Point;
-                shadowImage.alphaIsTransparency = true;
-            }
-        }
-
-        public struct CharacterSheet
-        {
-            public Sprite name;
-            public Sprite avatarA;
-            public Sprite avatarB;
-            public Sprite icon;
-            public Sprite[] roundIcons;
-
-            Texture2D source;
-
-            public CharacterSheet(byte [] pngData)
-            {
-                source = new Texture2D(1200, 950, TextureFormat.ARGB32, false);
-                source.LoadImage(pngData);
-                source.filterMode = FilterMode.Point;
-                source.alphaIsTransparency = true;
-                
-
-                name = Sprite.Create(source, new Rect(540, 83, 600, 67), new Vector2(300, 33.5f), 100);
-                avatarA = Sprite.Create(source, new Rect(0, 150, 600, 800), new Vector2(300, 400), 100);
-                avatarB = Sprite.Create(source, new Rect(600, 150, 600, 800), new Vector2(300, 400), 100);
-                icon = Sprite.Create(source, new Rect(0, 0, 150, 150), new Vector2(75, 75), 100);
-
-                roundIcons = new Sprite[3];
-                roundIcons[0] = Sprite.Create(source, new Rect(150, 20, 130, 130), new Vector2(65, 65), 100);
-                roundIcons[1] = Sprite.Create(source, new Rect(280, 20, 130, 130), new Vector2(65, 65), 100);
-                roundIcons[2] = Sprite.Create(source, new Rect(410, 20, 130, 130), new Vector2(65, 65), 100);
-            }
-        }
+        /// <summary> Height of the sprite sheet </summary>
+        const int ImageHeight = 950;
 
         // --- Static Properties ------------------------------------------------------------------------
         // --- Static Methods ---------------------------------------------------------------------------
         // -----------------------------------------------------------------------------------
         // --- Properties -------------------------------------------------------------------------------
-        CharacterSheet characterSheet;
-        
-        public Texture2D[] roundImage;
-        public Texture2D[] shadowImage;
+        /// <summary> Sprite with the character's name </summary>
+        public Sprite name { get; private set; }
 
-        Header header;
+        /// <summary> Sprite with the character's avatar - selected </summary>
+        public Sprite avatarA { get; private set; }
+
+        /// <summary> Sprite with the character's avatar - confirmed </summary>
+        public Sprite avatarB { get; private set; }
+
+        /// <summary> Sprite with the character's icon for the selection grid </summary>
+        public Sprite icon { get; private set; }
+
+        /// <summary> Cleared round preview images </summary>
+        public Sprite[] roundIcons { get; private set; }
+
+        /// <summary> Source texture </summary>
+        Texture2D source;
 
         // --- Methods ----------------------------------------------------------------------------------
         // -----------------------------------------------------------------------------------	
-        
-
-        // -----------------------------------------------------------------------------------	
-        public void LoadCore(string filename)
+        public CharacterSheet(byte[] pngData)
         {
-            byte[] data = IllogicGate.Data.EncryptedFile.ReadBytes(filename);
-            MemoryStream ms = new MemoryStream(data);
-            BinaryReader br = new BinaryReader(ms);
+            source = new Texture2D(ImageWidth, ImageHeight, TextureFormat.ARGB32, false);
+            source.LoadImage(pngData);
+            source.filterMode = FilterMode.Point;
+            source.alphaIsTransparency = true;
 
-            // load header
-            header = new Header();
-            header.Load(br);
+            // these values are taken directly from the sprite
+            // sheet. If the sprite sheet layout changes, fix
+            // these values too
+            name = Sprite.Create(source, new Rect(540, 83, 600, 67), new Vector2(300, 33.5f), 100);
+            avatarA = Sprite.Create(source, new Rect(0, 150, 600, 800), new Vector2(300, 400), 100);
+            avatarB = Sprite.Create(source, new Rect(600, 150, 600, 800), new Vector2(300, 400), 100);
+            icon = Sprite.Create(source, new Rect(0, 0, 150, 150), new Vector2(75, 75), 100);
 
-            //selectionMenu.name = DeserializeTexture(br);
+            roundIcons = new Sprite[3];
+            roundIcons[0] = Sprite.Create(source, new Rect(150, 20, 130, 130), new Vector2(65, 65), 100);
+            roundIcons[1] = Sprite.Create(source, new Rect(280, 20, 130, 130), new Vector2(65, 65), 100);
+            roundIcons[2] = Sprite.Create(source, new Rect(410, 20, 130, 130), new Vector2(65, 65), 100);
+        }
+    }
+
+    // --- Class Declaration ------------------------------------------------------------------------
+    /// <summary>
+    /// Holds the images for a given game round, both base image and shadow
+    /// </summary>
+    public class RoundData
+    {
+        // --- Events -----------------------------------------------------------------------------------
+        // --- Constants --------------------------------------------------------------------------------
+        // --- Static Properties ------------------------------------------------------------------------
+        // --- Static Methods ---------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------
+        // --- Properties -------------------------------------------------------------------------------
+        /// <summary> Base image </summary>
+        public Texture2D baseImage { get; private set; }
+
+        /// <summary> Shadow image </summary>
+        public Texture2D shadowImage { get; private set; }
+
+        // --- Methods ----------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------	
+        /// <summary>
+        /// Creates the textures needed for the round
+        /// </summary>
+        /// <param name="pngBase">PNG data for the base image</param>
+        /// <param name="pngShadow">PNG data for the shadow </param>
+        public RoundData(byte[] pngBase, byte[] pngShadow)
+        {
+            baseImage = new Texture2D(PlayArea.ImageWidth, PlayArea.ImageHeight, TextureFormat.ARGB32, false);
+            baseImage.LoadImage(pngBase);
+            baseImage.filterMode = FilterMode.Point;
+
+            shadowImage = new Texture2D(PlayArea.ImageWidth, PlayArea.ImageHeight, TextureFormat.Alpha8, false);
+            shadowImage.LoadImage(pngShadow);
+            shadowImage.filterMode = FilterMode.Point;
+            shadowImage.alphaIsTransparency = true;
+        }
+    }
+
+    // --- Class Declaration ------------------------------------------------------------------------
+    /// <summary>
+    /// Saves and loads character datafiles.
+    /// </summary>
+    public class CharacterDataFile
+    {
+        // --- Constants --------------------------------------------------------------------------------
+        // unshuffled key is "8f8c06bdfc41a468ad379e34791cbf78581295ed90abf6118ea30082b561"
+        const string ShuffledKey = @"9h;d29cfid64b699cg49<f578;4ddi8:8935:7he;3bdi7349gd42394e684";
+
+        /// <summary> File version 1 </summary>
+        const byte Version1 = 1;
+
+        /// <summary> A file entry in the data file </summary>
+        struct Entry
+        {
+            /// <summary> offset from the start of the file </summary>
+            public int offset;
+
+            /// <summary> length in bytes </summary>
+            public int length;
+
+            public Entry(int offset = 0, int length = 0)
+            {
+                this.offset = offset;
+                this.length = length;
+            }
+
+            public Entry(BinaryReader br)
+            {
+                offset = br.ReadInt32();
+                length = br.ReadInt32();
+            }
+            public void Save(BinaryWriter bw)
+            {
+                bw.Write(offset);
+                bw.Write(length);
+            }
         }
 
-        // -----------------------------------------------------------------------------------	
-        public void LoadRound(string filename, int round)
+        /// <summary> Data file header. This might change with versions </summary>
+        struct Header
         {
-            byte[] data = IllogicGate.Data.EncryptedFile.ReadBytes(filename);
-            MemoryStream ms = new MemoryStream(data);
-            BinaryReader br = new BinaryReader(ms);
+            public byte version;
+            public Entry characterSheet;
+            public Entry[] roundBase;
+            public Entry[] roundShadow;
 
-            Header header = new Header();
-            header.Load(br);
+            public Header(byte version)
+            {
+                this.version = version;
+                characterSheet = new Entry();
+                roundBase = new Entry[3];
+                roundShadow = new Entry[3];
+            }
+
+            public Header(BinaryReader br)
+            {
+                version = br.ReadByte();
+                characterSheet = new Entry(br);
+                roundBase = new Entry[3];
+                roundShadow = new Entry[3];
+
+                for (int i = 0; i < roundBase.Length; i++)
+                {
+                    roundBase[i] = new Entry(br);
+                    roundShadow[i] = new Entry(br);
+                }
+            }
+
+            public void Save(BinaryWriter bw)
+            {
+                bw.Write(version);
+                characterSheet.Save(bw);
+                for (int i = 0; i < roundBase.Length; i++)
+                {
+                    roundBase[i].Save(bw);
+                    roundShadow[i].Save(bw);
+                }
+            }
         }
 
-        // -----------------------------------------------------------------------------------	
-        void SerializeTexture(Texture2D tex, BinaryWriter bw)
+        // --- Static Methods ---------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------
+#if UNITY_EDITOR
+        /// <summary>
+        /// Creates a file with the character sheet and each round image
+        /// Only available in editor mode
+        /// encrypting them separately
+        /// </summary>
+        /// <param name="filename">File to save to</param>
+        /// <param name="charSheetFile"> PNG file with the character sheet </param>
+        /// <param name="roundFiles"> PNG files for each round (base, shadow) </param>
+        public static void CreateFile(string filename, string charSheetFile, string [,] roundFiles)
         {
-            bw.Write(tex.width);
-            bw.Write(tex.height);
-            bw.Write((int)tex.format);
-            byte[] data = tex.EncodeToPNG();
-            bw.Write(data.Length);
+            BinaryWriter bw = new BinaryWriter(File.Open(filename, FileMode.Create));
+            BlowFishCS.BlowFish blowfish = new BlowFishCS.BlowFish(IllogicGate.Data.EncryptedFile.RestoreKey(ShuffledKey));
+            
+            // save an empty header to make space for it
+            Header header = new Header(Version1);
+            header.Save(bw);
+
+            byte[] data;
+
+            // encrypt and save the character sheet file
+            data = File.ReadAllBytes(charSheetFile);
+            data = blowfish.Encrypt_ECB(data);
+            header.characterSheet = new Entry((int)bw.BaseStream.Position, data.Length);
             bw.Write(data);
+
+            // encrypt and save round images
+            for (int i = 0; i < 3; i++)
+            {
+                data = File.ReadAllBytes(roundFiles[i, 0]);
+                data = blowfish.Encrypt_ECB(data);
+                header.roundBase[i] = new Entry((int)bw.BaseStream.Position, data.Length);
+                bw.Write(data);
+
+                data = File.ReadAllBytes(roundFiles[i, 1]);
+                data = blowfish.Encrypt_ECB(data);
+                header.roundShadow[i] = new Entry((int)bw.BaseStream.Position, data.Length);
+                bw.Write(data);
+            }
+
+            // rewind and overwrite header
+            bw.Seek(0, SeekOrigin.Begin);
+            header.Save(bw);
+
+            bw.Close();
+        }
+
+#endif
+        // -----------------------------------------------------------------------------------	
+        /// <summary>
+        /// Loads and decrypts a character sheet from file
+        /// </summary>
+        /// <param name="filename">File to load it from</param>
+        public static CharacterSheet LoadCharacterSheet(string filename)
+        {
+            BinaryReader br = new BinaryReader(File.Open(filename, FileMode.Open));
+            BlowFishCS.BlowFish blowfish = new BlowFishCS.BlowFish(IllogicGate.Data.EncryptedFile.RestoreKey(ShuffledKey));
+
+            Header header = new Header(br);
+
+            // read and decrypt the character sheet
+            br.BaseStream.Seek(header.characterSheet.offset, SeekOrigin.Begin);
+            byte[] pngData = br.ReadBytes(header.characterSheet.length);
+            pngData = blowfish.Decrypt_ECB(pngData);
+            br.Close();
+
+            return new CharacterSheet(pngData);
         }
 
         // -----------------------------------------------------------------------------------	
-        Texture2D DeserializeTexture( BinaryReader br)
+        /// <summary>
+        /// Loads the images for a given round
+        /// </summary>
+        /// <param name="filename">File name to load the rounds from</param>
+        /// <param name="round">round to load (0 to 2)</param>
+        public static RoundData LoadRound(string filename, int round)
         {
-            int w = br.ReadInt32();
-            int h = br.ReadInt32();
-            TextureFormat format = (TextureFormat)br.ReadInt32();
-            byte [] data = br.ReadBytes(br.ReadInt32());
+            BinaryReader br = new BinaryReader(File.Open(filename, FileMode.Open));
+            BlowFishCS.BlowFish blowfish = new BlowFishCS.BlowFish(IllogicGate.Data.EncryptedFile.RestoreKey(ShuffledKey));
 
-            Texture2D tex = new Texture2D(w, h, format, false);
-            tex.LoadImage(data);
+            Header header = new Header(br);
 
-            // we assume these values to be correct
-            tex.filterMode = FilterMode.Point;
-            tex.alphaIsTransparency = true;
+            // read and decrypt the round image and shadow
+            br.BaseStream.Seek(header.roundBase[round].offset, SeekOrigin.Begin);
+            byte[] pngBase = br.ReadBytes(header.roundBase[round].length);
+            pngBase = blowfish.Decrypt_ECB(pngBase);
 
-            return tex;
+            br.BaseStream.Seek(header.roundShadow[round].offset, SeekOrigin.Begin);
+            byte[] pngShadow = br.ReadBytes(header.roundShadow[round].length);
+            pngShadow = blowfish.Decrypt_ECB(pngShadow);
+
+            br.Close();
+
+            return new RoundData(pngBase, pngShadow);
         }
 
+
+        // --- Properties -------------------------------------------------------------------------------
+        // --- Methods ----------------------------------------------------------------------------------
     }
 }
