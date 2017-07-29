ea1c36d5dec60a8aa0e269e85511655f723bb3ff
Arkaid D <aterfelis@gmail.com>
2017-07-29 13:14:34 +0900

    BossTracker now correctly shows where boss is on the screen edges

--- Assets/Scripts/Game/UI/BossTracker.cs
+++ Assets/Scripts/Game/UI/BossTracker.cs
@@ -15,22 +15,24 @@ namespace Jintori.Game
         // -----------------------------------------------------------------------------------
         // --- Inspector --------------------------------------------------------------------------------
         [SerializeField]
-        Transform up = null;
+        RectTransform up = null;
 
         [SerializeField]
-        Transform down = null;
+        RectTransform down = null;
 
         [SerializeField]
-        Transform left = null;
+        RectTransform left = null;
 
         [SerializeField]
-        Transform right = null;
+        RectTransform right = null;
 
         // --- Properties -------------------------------------------------------------------------------
         /// <summary> Boss to track </summary>
         Enemy boss = null;
 
+        /// <summary> Collider used to detect whether the boss is in or out of view </summary>
         new BoxCollider2D collider = null;
+
         // --- MonoBehaviour ----------------------------------------------------------------------------
         // -----------------------------------------------------------------------------------	
         void Start()
@@ -82,18 +84,78 @@ namespace Jintori.Game
         }
 
         // -----------------------------------------------------------------------------------	
+        float SignedAngle(Vector2 from, Vector2 to)
+        {
+            return (Vector2.SignedAngle(from, to) + 360) % 360;
+        }
+
+        // -----------------------------------------------------------------------------------	
         IEnumerator TrackCoroutine()
         {
+            // calculate the angles between the
+            // center of the screen and each corner
+            float w = Screen.width * 0.5f;
+            float h = Screen.height * 0.5f;
+            Vector2 tr = new Vector2( w,  h);
+            Vector2 tl = new Vector2(-w,  h);
+            Vector2 br = new Vector2( w, -h);
+            Vector2 bl = new Vector2(-w, -h);
+
+            // each angle is measured from the top-right corner
+            float tp = SignedAngle(tr, tl);
+            float lt = SignedAngle(tr, bl);
+            float bt = SignedAngle(tr, br);
+
             while (boss != null)
             {
                 yield return null;
 
+                up.gameObject.SetActive(false);
+                down.gameObject.SetActive(false);
+                left.gameObject.SetActive(false);
+                right.gameObject.SetActive(false);
+                
                 // no need to track here
                 if (collider.IsTouching(boss.collider))
-                    continue;
+                    continue;           
 
-                // which side should we turn on?
-                print(boss.transform.position);
+                // get boss position in viewport coordinates
+                Vector2 bossPos = Camera.main.WorldToViewportPoint(boss.transform.position);
+                bossPos.x = Mathf.Clamp01(bossPos.x);
+                bossPos.y = Mathf.Clamp01(bossPos.y);
+
+                // calculate angle respect to top-left
+                float angle = SignedAngle(tr, boss.transform.position);
+
+                // show correct arrow and place it at boss level
+                if (angle < tp)
+                {
+                    bossPos.y = 1;
+                    up.gameObject.SetActive(true);
+                    up.anchorMin = bossPos;
+                    up.anchorMax = bossPos;    
+                }
+                else if (angle < lt)
+                {
+                    bossPos.x = 0;
+                    left.gameObject.SetActive(true);
+                    left.anchorMin = bossPos;
+                    left.anchorMax = bossPos;
+                }
+                else if (angle < bt)
+                {
+                    bossPos.y = 0;
+                    down.gameObject.SetActive(true);
+                    down.anchorMin = bossPos;
+                    down.anchorMax = bossPos;
+                }
+                else
+                {
+                    bossPos.x = 1;
+                    right.gameObject.SetActive(true);
+                    right.anchorMin = bossPos;
+                    right.anchorMax = bossPos;
+                }
             }
         }
     }
