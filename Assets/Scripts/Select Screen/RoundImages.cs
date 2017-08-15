4569650dd7d12da5b0e4c3fb12f93112c0a2b4e7
Francisco Menendez <menendez.f@cave.co.jp>
2017-08-15 13:35:33 +0900

    Added support for 3/4 round variable characters

--- Assets/Scripts/Select Screen/RoundImages.cs
+++ Assets/Scripts/Select Screen/RoundImages.cs
@@ -21,6 +21,9 @@ namespace Jintori.SelectScreen
         [SerializeField]
         Sprite lockedSprite = null;
 
+        [SerializeField]
+        Sprite unavailableSprite = null;
+
         // --- Properties -------------------------------------------------------------------------------
         CharacterFile.File characterFile;
         Data.CharacterStats stats;
@@ -46,12 +49,18 @@ namespace Jintori.SelectScreen
                 IllogicGate.SoundManager2D.instance.PlaySFX("ui_accept");
                 Overlay.instance.roundImageViewer.Show(characterFile, idx);
             }
-            else
+            else if (idx < characterFile.availableRounds)
             {
                 IllogicGate.SoundManager2D.instance.PlaySFX("ui_cancel");
                 string diff = idx == 3 ? "hard" : "normal";
                 string msg = string.Format("Clear round {0} in {1} difficulty to unlock this image", idx + 1, diff);
                 Overlay.instance.messagePopup.Show(msg.ToUpper(), "IMAGE LOCKED");
+            }
+            else
+            {
+                IllogicGate.SoundManager2D.instance.PlaySFX("ui_cancel");
+                string msg = string.Format("Character does not have round {0}", idx + 1);
+                Overlay.instance.messagePopup.Show(msg.ToUpper(), "UNAVAILABLE");
             }
         }
 
@@ -72,8 +81,10 @@ namespace Jintori.SelectScreen
             {
                 if (stats.rounds[i].cleared)
                     icons[i].previewIcon.sprite = characterFile.baseSheet.roundIcons[i];
-                else
+                else if (i < characterFile.availableRounds)
                     icons[i].previewIcon.sprite = lockedSprite;
+                else
+                    icons[i].previewIcon.sprite = unavailableSprite;
             }
         }
     }
