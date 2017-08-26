d69ca7992bec538ba63415ce0eeda5464951a98d
Francisco Menendez <menendez.f@cave.co.jp>
2017-08-26 09:38:07 +0900

    Added Kuroak's 1st track (unplayed)

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -18,43 +18,14 @@ public class Test : MonoBehaviour
     // --- Static Methods ---------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------
     // --- Inspector --------------------------------------------------------------------------------
-    [SerializeField]
-    Collider2D target;
 
     // --- Properties -------------------------------------------------------------------------------
-    Vector2 velocity = Vector2.one * -75;
 
     // --- MonoBehaviour ----------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------	
     private void Start()
     {
-        StartCoroutine(FixedUpdateCoroutine());
-    }
-    // -----------------------------------------------------------------------------------	
-    IEnumerator FixedUpdateCoroutine()
-    {
-        YieldInstruction wffu = new WaitForFixedUpdate();
-        while(true)
-        {
-            print("FixedUpdateCoroutine");
-            yield return null;
-        }
-    }
-    // -----------------------------------------------------------------------------------	
-    private void FixedUpdate()
-    {
-        print("FixedUpdate");
-        //transform.position = transform.position + (Vector3)velocity * Time.fixedDeltaTime;
-        /*
-        Collider2D col = GetComponent<Collider2D>();
-        if (col.IsTouching(target))
-            print("boop");
-            */
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        velocity = Vector2.Reflect(velocity, collision.contacts[0].normal);
     }
     // --- Methods ----------------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------	
