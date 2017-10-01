6831fa6736a46b7fbe779a74fb29da1e7e54eb40
Arkaid D <aterfelis@gmail.com>
2017-10-01 11:31:26 +0900

    Added Filter UI (not working)

---
+++ Assets/Scripts/Select Screen/Troggle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jintori.SelectScreen
+{
+    // --- Class Declaration ------------------------------------------------------------------------
+    /// <summary>
+    /// A troggle is a toogle with three values
+    /// </summary>
+    public class Troggle : Button
+    {
+        // --- Events -----------------------------------------------------------------------------------
+        // --- Constants --------------------------------------------------------------------------------
+        public enum Value
+        {
+            Yes,
+            No,
+            None,
+        }
+
+        static readonly Color NoColor = new Color32(212, 50, 50, 255);
+        static readonly Color YesColor = new Color32(45, 154, 50, 255);
+
+        // --- Static Properties ------------------------------------------------------------------------
+        // --- Static Methods ---------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------
+        // --- Inspector --------------------------------------------------------------------------------
+
+        // --- Properties -------------------------------------------------------------------------------
+        Image target;
+
+        public Value value { get { return _value; } set { SetValue(value); } }
+        Value _value;
+
+        // --- MonoBehaviour ----------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------	
+        protected override void Start()
+        {
+            base.Start();
+            target = transform.Find("Background/Checkmark").GetComponent<Image>();
+            onClick.AddListener(OnClick);
+        }
+        // --- Methods ----------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------	
+        void SetValue(Value value)
+        {
+            _value = value;
+            switch(value)
+            {
+                case Value.Yes: target.color = YesColor; break;
+                case Value.No: target.color = NoColor; break;
+                case Value.None: target.color = Color.clear; break;
+            }
+        }
+
+        // -----------------------------------------------------------------------------------	
+        void OnClick()
+        {
+            value =  (Value)(((int)value + 1) % 3);
+        }
+    }
+}
